ed12414edfc0811f276eaf32d6cf03ac4a421d0b
cliffgor <clierozigy@gmail.com>
2017-02-21 12:23:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$("h1").click(function() {
+      alert("This is a header.");
+    });
+
+$("p").click(function() {
+      alert("This is a paragraph.");
+    });
+
+$("img").click(function() {
+      alert("This is an image.");
+      alert("This is an image.");
+    });
